72dd178b6d27086993a6771cce33222b55054f0a
gentlemanyc <462086630@qq.com>
2015-06-29 17:40:22 +0800

    rename packages,android resolve bugs that ImageView and TextView  not in center

---
+++ src/cc/core/bottomnavtabview/MainActivity.java
@@ -0,0 +1,35 @@
+package cc.core.bottomnavtabview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    findViewById(R.id.button1).setOnClickListener(new OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, TabActivity.class);
+        intent.putExtra("isInit3", true);
+        startActivity(intent);
+      }
+    });
+    findViewById(R.id.button2).setOnClickListener(new OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, TabActivity.class);
+        intent.putExtra("isInit3", false);
+        startActivity(intent);
+      }
+    });
+  }
+
+}
